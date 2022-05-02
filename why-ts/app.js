b89eb0347e0d18b6c4272e615e16fc7afcf80d5a
letminjae <cmjj0824@naver.com>
2022-05-02 14:56:29 +0900

    why Typescript?

--- why-ts/app.js
+++ why-ts/app.js
@@ -13,9 +13,13 @@ function startApp() {
   axios
     .get(url)
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user)
+      username.innerHTML = user.name;
+      email.innerHTML = user.email;
+      address.innerHTML = user.address.city;
     })
     .catch(function (error) {
       console.log(error);
