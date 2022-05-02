c613664054bbc2a5db945633bb4c008d36df703c
letminjae <cmjj0824@naver.com>
2022-05-02 15:49:33 +0900

    getting started

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,4 +1,12 @@
-function sum(a, b) {
+/**
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+
+// @ts-check
+
+ function sum(a, b) {
     return a + b;
 }
-sum(10, 20);
+
+sum(10, '20');
